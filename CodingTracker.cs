4992001df9cbb5a929ecf6d72f562e3f213ac2c4
Ruksan AC <ruksanarrashid@gmail.com>
2026-02-11 18:09:09 +0530

    Configured database connection

---
+++ CodingTracker.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CodingTracker.Ruksan12
+{
+    internal class CodingTracker
+    {
+        public static void Main(string[] args)
+        {
+            string connectionString = "Data Source=coding-tracker.db";
+
+
+        }
+    }
+}
---
+++ DatabaseManager.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.Data.Sqlite;
+using Dapper;
+
+namespace CodingTracker.Ruksan12
+{
+    internal class DatabaseManager
+    {
+        private readonly string _connectionString;
+        public DatabaseManager(string connectionString) 
+        { 
+            _connectionString = connectionString;
+        }
+
+        public void CreateDatabase()
+        {
+            using (var connection = new SqliteConnection(_connectionString))
+            {
+                connection.Open();
+                string createTableQuery = @"
+                    CREATE TABLE IF NOT EXISTS CodingSessions (
+                        Id INTEGER PRIMARY KEY AUTOINCREMENT,
+                        StartTime TEXT NOT NULL,
+                        EndTime TEXT NOT NULL,
+                        Duration INTEGER NOT NULL
+                    )";
+                using (var command = new SqliteCommand(createTableQuery, connection))
+                {
+                    command.ExecuteNonQuery();
+                }
+            }
+        }
+    }
+}
